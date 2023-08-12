b1387d7fd6fd5814ba5310410b739d7edb82adcd
karen0830 <lopezkaren43567@gmail.com>
2023-08-12 16:10:33 -0500

    First commit

---
+++ js/InfoPelis.js
@@ -0,0 +1,142 @@
+export const peliculas = [
+    {
+        trailer: "oBmazlyP220",
+        titulo: "Spider-Man: Across the Spider-Verse",
+        imagen: "https://cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/SJZWB3NSG5B37MNPEGIPEOJDUQ.png",
+        genero: "Animación",
+        descripcion: "Across The Spider-Verse, a lo largo de un poco más de dos horas, nos presenta una historia en la que Miles Morales, ya siendo un adolescente más grande, está cruzando por esas complicadas olas entre ser el superhéroe de Nueva York y aplicar como estudiante prodigio a un cupo universitario de alto nivel."
+    },
+    {
+        trailer: "NjBGzJ5FFmI",
+        titulo: "Transformers: Rise of the Beasts",
+        imagen: "https://image.tmdb.org/t/p/original/2vFuG6bWGyQUzYS9d69E5l85nIz.jpg",
+        genero: ["Acción", "Ciencia ficción"],
+        descripcion: "Cuando surge una nueva amenaza capaz de destruir todo el planeta, Optimus Prime y los Autobots deben unirse a una poderosa facción conocida como los Maximals. Con el destino de la humanidad en juego, los humanos Noah y Elena harán lo que sea necesario para ayudar a los Transformers mientras se involucran en la batalla final para salvar la Tierra."
+    },
+    {
+        trailer: "zh4KhVSMwtQ",
+        titulo: "Barbie",
+        imagen: "https://image.tmdb.org/t/p/w500/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg",
+        genero: "Comedia",
+        descripcion: "Barbie y Ken están pasando el mejor momento de sus vidas en el colorido y aparentemente perfecto mundo de Barbie Land. Sin embargo, cuando tienen la oportunidad de ir al mundo real, pronto descubren las alegrías y los peligros de vivir entre humanos."
+    },
+    {
+        trailer: "av-9lvBdZ0k",
+        titulo: "The Flash",
+        imagen: "https://image.tmdb.org/t/p/w500/rktDFPbfHfUbArZ6OOOKsXcv0Bm.jpg",
+        genero: "Ciencia ficción",
+        descripcion: "Cuando su intento de salvar a su familia altera el futuro sin darse cuenta, Barry Allen queda atrapado en una realidad en la que el general Zod ha regresado y no hay superhéroes a los que acudir. Para salvar el mundo en el que se encuentra y volver al futuro que él conoce, la única esperanza de Barry es correr por su vida. Pero, ¿será suficiente hacer el último sacrificio para reiniciar el universo."
+    },
+    {
+        trailer: "XgNtxm5aTv0",
+        titulo: "The Flood",
+        imagen: "https://www.lavanguardia.com/peliculas-series/images/movie/poster/2023/7/w1280/mvjqqklMpHwOxc40rn7dMhGT0Fc.jpg",
+        genero: ["Ciencia ficción", "Acción"],
+        descripcion: "Una horda de caimanes hambrientos gigantes se desata sobre un grupo de prisioneros en tránsito y sus guardias después de que un huracán masivo inunde Luisiana."
+    },
+    {
+        trailer: "dG91B3hHyY4",
+        titulo: "Meg 2: The Trench",
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCUfCzB6m6epRTscL-QOY7FAhR_07pMo3T8KBqkBqCiEpPtUQH6xOMRg06Is1t7I0cFDU&usqp=CAU",
+        genero: ["Aventura", "Acción"],
+        descripcion: "Una inmersión exploratoria en las profundidades más profundas del océano de un audaz equipo de investigación se convierte en un caos cuando una operación minera malévola amenaza su misión y los obliga a una batalla de alto riesgo por la supervivencia."
+    },
+    {
+        trailer: "uNZu5dNck9Y",
+        titulo: "Insidious: The Red Door",
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr0O7KEs7Ivt_sYMMrw-GAbQ08pA5d3SBTZbOFYvBbvQ&s",
+        genero: "Terror",
+        descripcion: "Para acabar con sus demonios de una vez por todas, Josh Lambert y Dalton Lambert, en edad universitaria, deben adentrarse más que nunca en El Más Allá, enfrentándose al oscuro pasado de su familia y a una multitud de terrores nuevos y más horribles que acechan detrás de la red. puerta."
+    },
+    {
+        trailer: "QtAlwa0xmHg",
+        titulo: "Hidden Strike",
+        imagen: "https://image.tmdb.org/t/p/original/dWvDlTkt9VEGCDww6IzNRgm8fRQ.jpg",
+        genero: "Acción",
+        descripcion: "Dos soldados de élite deben escoltar a los civiles a través de una serie de disparos y explosiones."
+    },
+    {
+        trailer: "uIvBFUYzeOc",
+        titulo: "The Little Mermaid",
+        imagen: "https://s3.amazonaws.com/nightjarprod/content/uploads/sites/253/2023/05/19122834/ym1dxyOk4jFcSl4Q2zmRrA5BEEN-scaled.jpg",
+        genero: "Ciencia ficción",
+        descripcion: "La más joven de las hijas del rey Tritón y la más desafiante, Ariel anhela saber más sobre el mundo más allá del mar, y mientras visita la superficie, se enamora del apuesto príncipe Eric. Con las sirenas prohibidas para interactuar con los humanos, Ariel hace una lidiar con la malvada bruja del mar, Úrsula, lo que le da la oportunidad de experimentar la vida en la tierra, pero finalmente pone su vida, y la corona de su padre, en peligro."
+    },
+    {
+        trailer: "ThmEKGQC_s8",
+        titulo: "バイオハザード：デスアイラン",
+        imagen: "https://i0.wp.com/themoviedb.org/t/p/w780/7drO1kYgQ0PnnU87sAnBEphYrSM.jpg",
+        genero: "Ciencia ficción",
+        descripcion: "En San Francisco, Jill Valentine está lidiando con un brote de zombis y un nuevo virus T, Leon Kennedy está tras la pista de un científico de DARPA secuestrado y Claire Redfield está investigando un pez monstruoso que está matando ballenas en la bahía. Chris Redfield y Rebecca Chambers, descubren el rastro de pistas de sus casos separados, todos convergen en el mismo lugar, la isla de Alcatraz, donde un nuevo mal se ha instalado y espera su llegada."
+    },
+    {
+        trailer: "FDhvbIqTQwI",
+        titulo: "Fast X",
+        imagen: "https://www.lavanguardia.com/peliculas-series/images/movie/backdrop/2023/5/w1280/4XM8DUTQb3lhLemJC51Jx4a2EuA.jpg",
+        genero: "Acción",
+        descripcion: "Durante muchas misiones y contra viento y marea imposibles, Dom Toretto y su familia han sido más astutos, nerviosos y vencieron a todos los enemigos en su camino. Ahora, se enfrentan al oponente más letal que jamás hayan enfrentado: una amenaza aterradora que emerge de las sombras de el pasado que está alimentado por la venganza de sangre, y que está decidido a destrozar esta familia y destruir todo, y a todos, que Dom ama, para siempre."
+    },
+    {
+        trailer: "FDhvbIqTQwI",
+        titulo: "Guardians of the Galaxy Vol. 3",
+        imagen: "https://davistheater.com/wp-content/uploads/2023/05/5YZbUmjbMa3ClvSW1Wj3D6XGolb-scaled.jpg",
+        genero: ["Acción", "Ciencia ficción"],
+        descripcion: "Peter Quill, todavía conmocionado por la pérdida de Gamora, debe reunir a su equipo a su alrededor para defender el universo y proteger a uno de los suyos. Una misión que, si no se completa con éxito, podría conducir al final de los Guardianes como los conocemos."
+    },
+    {
+        trailer: "OzAoGlARPFQ",
+        titulo: "Miraculous - le film",
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT67sGZ5BBo-m_anGatt5zrCz2AsPB5sqWqPwbwKnIURA&s",
+        genero: "Animación",
+        descripcion: "La vida de una adolescente parisina ordinaria, Marinette, se vuelve sobrehumana cuando se convierte en Ladybug. Dotada de poderes mágicos de creación, Ladybug debe unirse a su opuesto, Cat Noir, para salvar París mientras un nuevo villano desata el caos en la ciudad."
+    },
+    {
+        trailer: "1QZNSrMTB",
+        titulo: "Fear the Night",
+        imagen: "https://image.tmdb.org/t/p/original/aLpQ3G2LRgXYNrQgUlo6AQRo9R6.jpg",
+        genero: "Acción",
+        descripcion: "Durante una despedida de soltera en una granja apartada de California, intrusos enmascarados lanzan un ataque brutal, obligando a ocho mujeres a luchar por sobrevivir. Dirigidas por Tess, una veterana militar con problemas, se unen para defenderse durante una noche desgarradora."
+    },
+    {
+        trailer: "qwo4fFji8rc",
+        titulo: "シン・仮面ライダー",
+        imagen: "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/iJ0UZaC7XW7BUpRQ7OLPZSms8Ou.jpg",
+        genero: "Acción",
+        descripcion: "Un hombre obligado a soportar el poder y despojado de humanidad. Una mujer escéptica de la felicidad. Takeshi Hongo, un Aumento creado por SHOCKER, y Ruriko Midorikawa, una rebelde de la organización, escapan mientras luchan contra asesinos. ¿Qué es la justicia? ¿Qué es el mal? Will ¿El final de esta violencia? A pesar de su poder, Hongo intenta seguir siendo humano. Junto con la libertad, Ruriko ha recuperado un corazón. ¿Qué caminos elegirán?"
+    },
+    {
+        trailer: "JdMxXytjzoc",
+        titulo: "Ruby Gillman, Teenage Kraken",
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuclEUbZUuINkgMHFwKL_cs23Lr9tl3sw-TdY6owuVSQ&s",
+        genero: "Animación",
+        descripcion: "Ruby Gillman, una estudiante de secundaria dulce y torpe, descubre que es descendiente directa de las reinas guerreras kraken. Los kraken han jurado proteger los océanos del mundo contra las sirenas vanidosas y hambrientas de poder. Destinadas a heredar el trono de ella. abuela al mando, Ruby debe usar sus nuevos poderes para proteger a los que más ama."
+    },
+    {
+        trailer: "IPDLodUE9gg",
+        titulo: "Justice League: Warworld",
+        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDzAtN-bEpurw4wKyqB9YvIwTwvPjd50RCuZggE-Drgw&s",
+        genero: "Animación",
+        descripcion: "Hasta ahora, la Liga de la Justicia ha sido una asociación flexible de individuos con superpoderes. Pero cuando son llevados a Warworld, un lugar de brutales combates de gladiadores sin fin, Batman, Superman, Wonder Woman y los demás deben unirse de alguna manera para formar una resistencia invencible. capaz de llevar a un planeta entero a la libertad."
+    },
+    {
+        trailer: "HTQLKvQU7Ho",
+        titulo: "ゾン100～ゾンビになるまでにしたい100のこと～",
+        imagen: "https://www.lavanguardia.com/peliculas-series/images/movie/backdrop/2023/8/w1280/aEVYGOIrhWDrNoZxVhhSazmRjeR.jpg",
+        genero: "Ciencia ficción",
+        descripcion: "Acosado por su jefe, trabajado día y noche, no es más que un dron corporativo. ¡Todo lo que necesita es un brote de zombis para que finalmente se sienta vivo!"
+    },
+    {
+        trailer: "z_HUm8JZlVs",
+        titulo: "River Wild",
+        imagen: "https://i.ytimg.com/vi/dveH9v_R644/maxresdefault.jpg",
+        genero: "Romance",
+        descripcion: "Sigue a un par de hermanos que se aman pero desconfían el uno del otro mientras se embarcan en un viaje de rafting en aguas bravas con un pequeño grupo. Uno de sus amigos de la infancia resulta ser más peligroso de lo que parece."
+    },
+    {
+        trailer: "izQA8C3emt4",
+        titulo: "Мавка: Лісова пісня",
+        imagen: "https://www.lavanguardia.com/peliculas-series/images/movie/backdrop/2023/3/w1280/14GEZCzCGhV7FMFaWi4Ec22Kcai.jpg",
+        genero: ["Animación", "Aventura"],
+        descripcion: "Mavka, un alma del bosque y su guardián, se enfrenta a una elección imposible entre el amor y su deber como guardiana del corazón del bosque, cuando se enamora de un humano: el talentoso joven músico Lukas."
+    },
+]
---
+++ js/Pelis.js
@@ -0,0 +1,171 @@
+// import { buscarElemento } from "../Busquedas.js";
+// import { peliculas } from "../Peliculas/InfoPelis.js";
+
+// JavaScript
+const section = document.getElementById('peliculas');
+const crear = () => {
+    const divGeneral = document.createElement('div');
+    divGeneral.id = 'divGeneral';
+
+    const button = document.createElement('button')
+    button.id = 'button'
+
+    const buttonLook = document.createElement('button')
+    buttonLook.id = 'buttonLook'
+    const h4Look = document.createElement('h4');
+    h4Look.id = 'h4Look';
+    h4Look.textContent = "Ver trailer"
+    buttonLook.appendChild(h4Look)
+
+    const div = document.createElement('div');
+    div.id = 'div';
+
+    const img = document.createElement('img');
+    img.id = 'img2';
+
+    const h1 = document.createElement('h4');
+    h1.id = 'h4';
+
+    const descripcion = document.createElement('p');
+    descripcion.id = 'description';
+
+    const leermas = document.createElement('button')
+    leermas.id = 'leermas';
+    leermas.textContent = 'Leer Más';
+
+    const div2 = document.createElement('div')
+    div2.id = 'div2'
+
+    div2.appendChild(descripcion);
+    div2.appendChild(leermas)
+
+    div.appendChild(h1);
+    div.appendChild(div2);
+
+    divGeneral.appendChild(img)
+    divGeneral.appendChild(div);
+    divGeneral.appendChild(buttonLook)
+
+    button.appendChild(divGeneral)
+
+    section.appendChild(button)
+
+    let key = 0;
+    let array = [div, divGeneral, img, h1, descripcion, section, leermas, div2, buttonLook, key];
+    return array
+}
+
+let matriz = [];
+let i = 0;
+peliculas.forEach(element => {
+    let array = crear();
+    array[2].src = element.imagen;
+    array[3].textContent = element.titulo;
+    array[4].textContent = element.descripcion;
+    array[9] = element.trailer;
+    console.log(array[9]);
+    matriz.push(array)
+    i++;
+})
+
+leerMasyMenos()
+// btnLookT();
+// buscarElemento();
+function leerMasyMenos() {
+    matriz.forEach(elemento => {
+        elemento[6].addEventListener('click', () => {
+            if (elemento[6].textContent == 'Leer Más') {
+                elemento[7].style = 'flex-direction: column;'
+                elemento[4].style = 'overflow: visible; width: 100%; white-space: wrap; '; // Cambia el overflow para que se muestre todo el texto
+                elemento[6].textContent = 'Leer Menos'
+                elemento[6].style = 'display: block; text-align: start;'; // Oculta el botón "Leer más" después de expandir el texto
+            } else {
+                elemento[4].style = 'white-space: nowrap; overflow: hidden; text-overflow: ellipsis;'
+                elemento[7].style = 'flex-direction: wrap;'
+                elemento[6].textContent = 'Leer Más'
+            }
+        });
+    })
+}
+
+// button look
+export function btnLookT() {
+    matriz.forEach(elemento => {
+        elemento[8].addEventListener('click', () => {
+            openModal(elemento[9]);
+        });
+    });
+}
+
+function openModal(videoId) {
+    const modal = createModal(videoId);
+    document.body.appendChild(modal);
+}
+
+function createModal(videoId) {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    const modalContent = document.createElement('div');
+    modalContent.classList.add('modal-content');
+
+    const closeButton = document.createElement('span');
+    closeButton.classList.add('close-button');
+    closeButton.innerHTML = '&times;';
+    closeButton.addEventListener('click', closeModal);
+
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://www.youtube.com/embed/${videoId}`;
+    iframe.setAttribute('allowfullscreen', true);
+    iframe.setAttribute('frameborder', 0);
+
+    modalContent.appendChild(closeButton);
+    modalContent.appendChild(iframe);
+    modal.appendChild(modalContent);
+
+    return modal;
+}
+
+function closeModal() {
+    const modal = document.querySelector('.modal');
+    if (modal) {
+        modal.remove();
+    }
+}
+
+// trailer
+const cardTrailer = () => {
+    const divGeneral = document.createElement('div');
+    divGeneral.id = 'divGeneralTrailer';
+
+    const div = document.createElement('div');
+    div.id = 'divDescriptionTrailerPeli';
+
+    const button = document.createElement('button')
+    button.id = 'buttonTrailer'
+    button.textContent = "X"
+
+    const iframe = document.createElement('iframe');
+    iframe.id = "iframeP";
+    iframe.src = `https://www.youtube.com/embed/iFl1cFggFb8`
+
+    const div2 = document.createElement('div');
+    div2.id = 'divTrailer';
+
+    const descripcion = document.createElement('p');
+    descripcion.id = 'description';
+
+    div.appendChild(button)
+    div.appendChild(iframe)
+
+    div2.appendChild(descripcion);
+
+    divGeneral.appendChild(div)
+    divGeneral.appendChild(div2);
+    const main = document.querySelector('main')
+    main.appendChild(divGeneral);
+    return divGeneral;
+}
+// Trailer
+// cardTrailer();
+
